cfbee32b9a46898cc398f482bc570b78a89ba5b6
kharioki <tony.kharioki@gmail.com>
2023-02-06 18:18:03 +0300

    header right component

--- app/_layout.js
+++ app/_layout.js
@@ -1,4 +1,6 @@
 import { Stack } from "expo-router";
+import { AntDesign } from '@expo/vector-icons';
+import { Alert } from "react-native";
 
 export default () => {
   return (
@@ -11,6 +13,14 @@ export default () => {
         headerTitleStyle: {
           fontWeight: 'bold'
         },
+        headerRight: () => (
+          <AntDesign
+            onPress={() => Alert.alert('More info...')}
+            name="infocirlceo"
+            size={22}
+            color="black"
+          />
+        ),
       }}
     >
       <Stack.Screen name="index" options={{ title: "Home" }} />
